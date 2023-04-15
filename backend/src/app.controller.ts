3a1f40a59a3a9536faa9f66929900d921d2df7aa
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2023-04-15 14:46:02 -0500

    add tts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get('tts')
+    async getTTS(@Query('text') text: string): Promise<Buffer> {
+        return await this.appService.getTTS(text);
     }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { getAllAudioUrls } from './utils/textToSpeech';
 
 @Injectable()
 export class AppService {
     getHello(): string {
         return 'Hello World!';
+    }
+
+    async getTTS(text: string): Promise<Buffer> {
+        const buf = await getAllAudioUrls(text);
+        return buf;
     }
 }
