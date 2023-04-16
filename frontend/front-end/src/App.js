d6e99134736b2f971fbedea89a5e8809d1de2d85
jeremu2907 <jeremu2907@gmail.com>
2023-04-16 00:18:22 -0500

    google

--- frontend/front-end/src/App.js
+++ frontend/front-end/src/App.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.css';
@@ -6,6 +6,22 @@ import Upload from "./components/UploadButton/Upload"
 import PostProcess from './pages/PostProcess';
 
 function App() {
+  function handleCallbackResponse(response){
+    console.log("JWD Token" + response.credential)
+  }
+
+  useEffect(() => {
+      /* global google */
+      google.accounts.id.initialize({
+          client_id: "767441415793-sq762l7n92ea8sq6bempmnvn6q6ebij6.apps.googleusercontent.com",
+          callback: handleCallbackResponse
+      })
+
+      google.accounts.id.renderButton(
+          document.getElementById("buttonDiv"),
+          {theme: "outline", size: "large"}
+      );
+  }, [])
   return (
     <div className="App">
       {/* <div className="App-header">
@@ -19,6 +35,7 @@ function App() {
       <Routes>
         <Route path="/" element={
           <div className="App-header">
+            {/* <div id="buttonDiv"></div> */}
             <h1>App Name</h1>
             <h2>Something to make the app sound epic</h2>
             <Upload/>
--- frontend/front-end/src/components/Menu/Menu.jsx
+++ frontend/front-end/src/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import {useState } from 'react';
 import './Menu.css'
+
 export default function Menu(){
     const [menuCollapse, setmenuCollapse] = useState(false);
     const btn = {
@@ -20,7 +21,7 @@ export default function Menu(){
         let i = 0;
         if(!menuCollapse){
             document.getElementById("menubtn").innerText="Menu";
-            for(; i<5; i++){
+            for(; i<4; i++){
                 document.getElementsByClassName("menuBtn")[i].style.width= "0px";
                 document.getElementsByClassName("menuBtn")[i].style.fontSize= "0px";
             }
@@ -28,13 +29,18 @@ export default function Menu(){
             setTimeout(() => {
                 document.getElementById("menubtn").innerText="Close Menu";
             }, 100);
-            for(; i<5; i++){
+            for(; i<4; i++){
                 document.getElementsByClassName("menuBtn")[i].style.width= "100%";
                 document.getElementsByClassName("menuBtn")[i].style.fontSize= "15px";
             }
         }
         setmenuCollapse(!menuCollapse);
     }
+
+    // const login = () => {
+    //     console.log("hi")
+    // }
+
     return(
         <div style={{position: "relative",textAlign: "start", display: "flex", flexDirection: "row", height: "50px"}}>
             <button id="menubtn" style={btn} onClick={menuClick}>Close Menu</button>
@@ -42,14 +48,15 @@ export default function Menu(){
             <Item text="Summary" />
             <Item text="Translate to Another Language"/>
             <Item text="Download" />
-            <Item text="Connect to YouTube" />
+            {/* <Item id="buttonDiv" text="Connect to YouTube"/> */}
+            <div id="buttonDiv" style={{alignSelf: "flex-end", justifySelf: "end", marginLeft: "auto"}}></div>
         </div>
     )
 }
 
 function Item (props) {
     return(
-        <button className="menuBtn">
+        <button className="menuBtn" onClick={props.callBack}>
             {props.text}
         </button>
     )
